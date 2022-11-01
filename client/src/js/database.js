9be8fd1639551cbab1156912a0413e64b7cf8f4f
Jorge Garit <jorgegarit@gmail.com>
2022-10-31 21:19:51 -0400

    added comments to database.js and will now work on the get and put functions

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,25 @@
 import { openDB } from 'idb';
+import 'regenerator-runtime/runtime';
 
 const initdb = async () =>
+  // this will create a new databse caled 'jate' this will be version 1
   openDB('jate', 1, {
+    // this will add the database schema if not already initialized
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      // new object store created for the data with key name of 'id'
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// PUT function for IndexedDb databse
+export const putDb = async (content) => 
 
-// TODO: Add logic for a method that gets all the content from the database
+// GET function for IndexedDb database
 export const getDb = async () => console.error('getDb not implemented');
 
 initdb();
